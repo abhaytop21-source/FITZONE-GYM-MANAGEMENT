9ff4dc9d50efda453be7c46ca55411dc23888b09
Abhay Dhas <abhaytop21@gmail.com>
2026-07-06 16:11:08 +0530

    added a script tag to include the members.js file at the end of the body in members.html.

---
+++ js/members.js
@@ -0,0 +1,123 @@
+let members = JSON.parse(localStorage.getItem("members")) || [];
+
+const memberTable = document.getElementById("memberTable");
+
+const addMemberBtn = document.getElementById("addMemberBtn");
+
+const memberName = document.getElementById("memberName");
+const memberPhone = document.getElementById("memberPhone");
+const memberPlan = document.getElementById("memberPlan");
+
+
+// Display Members
+function displayMembers() {
+
+    memberTable.innerHTML = "";
+
+    members.forEach((member, index) => {
+
+        memberTable.innerHTML += `
+
+        <tr>
+
+            <td>${index + 1}</td>
+
+            <td>${member.name}</td>
+
+            <td>${member.phone}</td>
+
+            <td>${member.plan}</td>
+
+            <td>Active</td>
+
+            <td>
+
+                <button onclick="deleteMember(${index})">
+                    Delete
+                </button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+}
+
+
+// Add Member
+
+addMemberBtn.addEventListener("click", () => {
+
+    if (
+        memberName.value === "" ||
+        memberPhone.value === "" ||
+        memberPlan.value === ""
+    ) {
+
+        alert("Please fill all fields.");
+
+        return;
+
+    }
+
+    members.push({
+
+        name: memberName.value,
+        phone: memberPhone.value,
+        plan: memberPlan.value
+
+    });
+
+    localStorage.setItem("members", JSON.stringify(members));
+
+    memberName.value = "";
+    memberPhone.value = "";
+    memberPlan.value = "";
+
+    displayMembers();
+
+});
+
+
+// Delete Member
+
+function deleteMember(index){
+
+    members.splice(index,1);
+
+    localStorage.setItem("members", JSON.stringify(members));
+
+    displayMembers();
+
+}
+
+
+// First Load
+
+displayMembers();
+
+const searchMember = document.getElementById("searchMember");
+
+searchMember.addEventListener("keyup", () => {
+
+    const value = searchMember.value.toLowerCase();
+
+    const rows = document.querySelectorAll("#memberTable tr");
+
+    rows.forEach(row => {
+
+        const text = row.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            row.style.display = "";
+        }
+        else{
+            row.style.display = "none";
+        }
+
+    });
+
+});
